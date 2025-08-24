51da3b777852b86501c63aa3f2f26f455aade367
crqch <48185508+crqch@users.noreply.github.com>
2025-08-24 22:21:50 +0200

    add telemetry construction page

---
+++ packages/client/src/routes/(app)/telemetry/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Construction } from "@lucide/svelte"
+
+</script>
+<div class="flex flex-col px-4 md:px-20 lg:px-40 py-10 mt-30 justify-center gap-8 items-center">
+    <Construction class="size-20 opacity-50" />
+    <h1 class="text-4xl font-semibold">This page is under construction</h1>
+</div>
