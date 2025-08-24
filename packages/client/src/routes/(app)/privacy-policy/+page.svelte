747b50879aa6deafcd90c5a0ab29691f206b2f15
crqch <48185508+crqch@users.noreply.github.com>
2025-08-24 14:32:38 +0200

    add privacy policy

---
+++ packages/client/src/routes/(app)/privacy-policy/+page.svelte
@@ -0,0 +1,75 @@
+<div class="flex flex-col lg:px-40 md:px-20 gap-12 px-4 py-10">
+  <div>
+    <h1 class="text-2xl md:text-4xl font-semibold">Privacy Policy</h1>
+    <p class="text-sm">
+      <strong class="font-semibold">Last Updated:</strong> August 24, 2025
+    </p>
+  </div>
+
+  <p class="text-lg leading-relaxed">
+    Your privacy is important to us. This Privacy Policy explains how we
+    collect, use, and protect your information when you visit our website.
+  </p>
+
+  <div>
+    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-primary">
+      1. Information We Collect
+    </h2>
+    <p class="leading-relaxed">
+      We do not collect any personally identifiable information from you. We do
+      not use cookies or other tracking technologies to gather your data.
+    </p>
+    <p class="mt-4 leading-relaxed">
+      The only information we may store is your language preference, which is
+      saved in your browser's local storage. This is for the sole purpose of
+      providing you with a consistent user experience and is not used to track
+      you or your activity.
+    </p>
+  </div>
+
+  <div>
+    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-primary">
+      2. Analytics
+    </h2>
+    <p class="leading-relaxed">
+      We use Cloudflare Web Analytics to understand how our website is used.
+      Cloudflare Web Analytics collects non-personally identifiable information
+      such as the pages you visit, the time you spend on our site, and the
+      referring website. This data is aggregated and does not identify you
+      individually. This helps us improve our website's performance and content.
+    </p>
+  </div>
+
+  <div>
+    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-primary">
+      3. Third-Party Links
+    </h2>
+    <p class="leading-relaxed">
+      Our website may contain links to other websites. We are not responsible
+      for the privacy practices of these other sites. We encourage you to read
+      the privacy policies of any website you visit.
+    </p>
+  </div>
+
+  <div>
+    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-primary">
+      4. Changes to This Privacy Policy
+    </h2>
+    <p class="leading-relaxed">
+      We may update this Privacy Policy from time to time. We will notify you of
+      any changes by posting the new Privacy Policy on this page.
+    </p>
+  </div>
+
+  <div>
+    <h2 class="text-2xl md:text-3xl font-bold mb-4 text-primary">
+      5. Contact Us
+    </h2>
+    <p class="leading-relaxed">
+      If you have any questions about this Privacy Policy, please contact us at:
+    </p>
+    <a class="href" href={`mailto:${atob("Y3JxY2guZGV2QGdtYWlsLmNvbQ")}`}
+      >{atob("Y3JxY2guZGV2QGdtYWlsLmNvbQ")}</a
+    >
+  </div>
+</div>
